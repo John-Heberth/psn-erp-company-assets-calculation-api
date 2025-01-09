9e1fb47b6c258d1eed0cee78f0ba3dda66f4585b
John <johnpereira.df@gmail.com>
2025-01-09 12:43:07 -0300

    Begin Project

---
+++ src/main/java/com/calculation/pension/erppension/psn_erp_company_assets_calculation_api/Application.java
@@ -0,0 +1,13 @@
+package com.calculation.pension.erppension.psn_erp_company_assets_calculation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculation/pension/erppension/psn_erp_company_assets_calculation_api/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.calculation.pension.erppension.psn_erp_company_assets_calculation_api;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
